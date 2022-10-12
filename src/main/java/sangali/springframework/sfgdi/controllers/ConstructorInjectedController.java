d5a4476e806cac9ad5facfb703c14c3383f136c2
Rodrigo <rodrigosangali@gmail.com>
2022-10-12 06:41:16 -0300

    Dependency Injection using Spring Framework

--- src/main/java/sangali/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/sangali/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,13 +1,11 @@
 package sangali.springframework.sfgdi.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import sangali.springframework.sfgdi.controllers.services.GreetingService;
 
 @Controller
 public class ConstructorInjectedController {
 
-    @Autowired
     private final GreetingService greetingService;
 
     public ConstructorInjectedController(GreetingService greetingService) {
