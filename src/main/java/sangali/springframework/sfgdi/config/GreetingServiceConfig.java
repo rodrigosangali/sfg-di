eb4787cc2030c20c40fd2d28e0db7ff0ff199af2
Rodrigo <rodrigo.sangali@gmail.com>
2022-11-14 08:44:23 -0300

    default profile

---
+++ src/main/java/sangali/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,46 @@
+package sangali.springframework.sfgdi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import sangali.springframework.sfgdi.services.*;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    i18nSpanishService i18nSpanishService(){
+        return  new i18nSpanishService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService(){
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService(){
+        return new SetterInjectedGreetingService();
+    }
+
+
+}
--- src/main/java/sangali/springframework/sfgdi/services/i18nSpanishService.java
+++ src/main/java/sangali/springframework/sfgdi/services/i18nSpanishService.java
@@ -1,10 +1,5 @@
 package sangali.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Profile({"ES", "default"})
-@Service("i18nService")
 public class i18nSpanishService implements GreetingService{
     @Override
     public String sayGreeting() {
